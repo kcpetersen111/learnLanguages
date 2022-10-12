a7eb4b39d5849a26a4cb46ee20afd35ca878aba9
Kaleb Petersen <kcpetersen111@gmail.com>
2022-10-11 22:26:46 -0600

    was not saving the image

--- python/keras/stableDiffusion.py
+++ python/keras/stableDiffusion.py
@@ -2,18 +2,20 @@ import time
 import keras_cv
 from tensorflow import keras 
 import matplotlib.pyplot as plt
+from PIL import Image
 
 
 model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
 
 images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=3)
 
-def plot_images(images):
-    plt.figure(figsize=(20, 20))
-    for i in range(len(images)):
-        ax = plt.subplot(1, len(images), i+1)
-        plt.imshow(images[i])
-        plt.axis("off")
+Image.fromarray(images[0]).save("test.png")
+# def plot_images(images):
+#     plt.figure(figsize=(20, 20))
+#     for i in range(len(images)):
+#         ax = plt.subplot(1, len(images), i+1)
+#         plt.imshow(images[i])
+#         plt.axis("off")
 
 
-plot_images(images)
+# plot_images(images)
