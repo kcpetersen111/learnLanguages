393541e8b20142b6903b98108d5f49703f5e1881
Kaleb Petersen <kcpetersen111@gmail.com>
2022-10-12 21:46:20 -0600

    looking into it more

--- python/keras/stableDiffusion.py
+++ python/keras/stableDiffusion.py
@@ -4,12 +4,16 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 from PIL import Image
 
-
+# fle = open("moby.txt")
+# st = fle.read()
+# fle.close()
 model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+name = "Snogaloren"
 
-images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=3)
+images = model.text_to_image(name, batch_size=3)
 
-Image.fromarray(images[0]).save("test.png")
+for i in range(len(images)):
+    Image.fromarray(images[i]).save("%s%s.png" % (name, i+1))
 # def plot_images(images):
 #     plt.figure(figsize=(20, 20))
 #     for i in range(len(images)):
